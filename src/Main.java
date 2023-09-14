636755ba43dc4c0d5c29e29985a0e3380b7e0818
Sam Epling <sgepling@icloud.com>
2023-09-14 15:03:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double price = 100.00;
+        double tax= price * .05;
+        System.out.println("The tax of an item that costs " + price + "$ is " + tax + "$");
+    }
+}
